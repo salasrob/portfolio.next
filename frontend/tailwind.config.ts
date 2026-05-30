dc901695271b9b05d59a8879620e42e6b2ed2135
Rob Salas <playerrsal@outlook.com>
2026-05-30 15:58:24 -0700

    Update nuget packages and tailwinds to v4

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography'
 
 const config: Config = {
   content: [
@@ -30,6 +31,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [typography],
 }
 export default config
